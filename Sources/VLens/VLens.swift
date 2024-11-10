244a8dc5a604381f5c52de3ceb5a91f02b1e8571
Mohamed Taher <mohamedt@silverkeytech.com>
2024-11-10 14:54:51 +0200

    Init Package

--- Sources/VLens/VLens.swift
+++ Sources/VLens/VLens.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public class VLens {
+    
+//    public static let shared = VLens()
+//    private init() {}
+    
+    public static func doSomething() {
+        print ("init-VLens-SDK")
+    }
+}
